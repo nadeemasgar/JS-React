a3f892237af810b8ae037301b73fda903cffb982
nadeemasgar1 <nadeem.asgar@tokopedia.com>
2022-03-30 13:59:50 +0530

    React Redux - Actions

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,1 +1,10 @@
-console.log("From index.js");
+const BUY_CAKE = "BUY_CAKE";
+
+// actionCreator function -> It's a function that return an action
+function actionCreator() {
+  // action as an object with type property
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}

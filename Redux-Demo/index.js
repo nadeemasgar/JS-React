3db74f45fa09da379abd6b7cdd8279b50a67ed41
nadeemasgar1 <nadeem.asgar@tokopedia.com>
2022-03-30 15:36:53 +0530

    React Redux - Reducers

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,10 +1,25 @@
 const BUY_CAKE = "BUY_CAKE";
 
 // actionCreator function -> It's a function that return an action
-function actionCreator() {
+function buyCake() {
   // action as an object with type property
   return {
     type: BUY_CAKE,
     info: "First redux action",
   };
 }
+
+// (previousState, action) => newState
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return { ...state, numOfCakes: state.numOfCakes - 1 };
+    default:
+      return state;
+  }
+};

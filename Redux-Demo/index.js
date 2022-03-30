996e152a3261f977453ec341b6bf2ba5d3d74f83
nadeemasgar1 <nadeem.asgar@tokopedia.com>
2022-03-30 16:18:20 +0530

    React Redux - Store

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
 
 // actionCreator function -> It's a function that return an action
@@ -23,3 +26,13 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("Initial State ", store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated State ", store.getState());
+});
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();

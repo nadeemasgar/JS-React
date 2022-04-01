b02b24ca58990763cac03754cc28781b5633c6bb
nadeemasgar1 <nadeem.asgar@tokopedia.com>
2022-04-01 15:37:13 +0530

    Using Redux Devtool Extension in React-Redux App

--- Redux-Demo/react-redux-demo3/src/Redux/store.js
+++ Redux-Demo/react-redux-demo3/src/Redux/store.js
@@ -2,6 +2,7 @@ import { createStore } from "redux"; // **
 import { combineReducers } from "redux"; // **
 import logger from "redux-logger"; // **
 import { applyMiddleware } from "redux"; // **
+import { composeWithDevTools } from "redux-devtools-extension";
 import cakeReducer from "./cake/cakeReducer";
 import iceCreamReducer from "./iceCream/iceCreamReducer";
 
@@ -10,7 +11,10 @@ const rootReducer = combineReducers({
   iceCream: iceCreamReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 console.log("Initial State", store.getState());
 // store.subscribe(() => console.log("Updated State", store.getState()));
 

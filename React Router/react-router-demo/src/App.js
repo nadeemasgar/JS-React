7595d6738eec81c3e0b9f2201fd97e311e1999fa
nadeemasgar1 <nadeem.asgar@tokopedia.com>
2022-05-02 01:48:53 +0530

    Active Links - NavLinks 2

--- React Router/react-router-demo/src/App.js
+++ React Router/react-router-demo/src/App.js
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-import Navbar from "./components/Navbar";
+import Navbar2 from "./components/Navbar2";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar2 />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
---
+++ React Router/react-router-demo/src/components/Navbar2.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar2() {
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "bold" : "normal",
+      textDecoration: isActive ? "none" : "underline",
+    };
+  };
+
+  return (
+    <nav>
+      <NavLink style={navLinkStyles} to="/">
+        Home
+      </NavLink>
+      <NavLink style={navLinkStyles} to="/about">
+        About
+      </NavLink> 
+    </nav>
+  );
+}
+
+export default Navbar2;

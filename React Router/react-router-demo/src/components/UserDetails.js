8268e784da89b1f19499364d910d2593fc53bbbf
nadeemasgar1 <nadeem.asgar@tokopedia.com>
2022-05-04 15:49:43 +0530

    URL Params

--- React Router/react-router-demo/src/components/UserDetails.js
+++ React Router/react-router-demo/src/components/UserDetails.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 function UserDetails() {
-  return (
-    <div>Details of the User</div>
-  )
+  const params = useParams();
+  const userId = params.userId;
+  return <div>Details of the User {userId}</div>;
 }
 
-export default UserDetails
+export default UserDetails;

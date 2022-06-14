0108221923957d6f44c30b185feeebd724c642b2
nadeemasgar1 <nadeem.asgar@tokopedia.com>
2022-06-14 23:41:52 +0530

    arrays, mixed arrays, object

--- TypeScript2/sandbox.js
+++ TypeScript2/sandbox.js
@@ -1,3 +1,4 @@
+/* 1. Types Basic */
 var character = "mario";
 var age = 30;
 var isBlackBelt = false;
@@ -12,3 +13,34 @@ var circ = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circ(7.5));
+/* 2. Objects & Arrays */
+// arrays
+var names = ["luigi", "yoshi", "yoshi"];
+names.push("toad");
+// names.push(3); // You can not insert a number into a arrays already containing String value
+// names[0] = 3   // You can not reassign a value into a arrays already with different types
+// Mixed Arrays
+var mixed = ["ken", 4, "chun-li", 8, 9]; // Array containing string and number
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+// Objects
+var ninja = {
+    name: "mario",
+    belt: "black",
+    age: 30
+};
+ninja.age = 40;
+ninja.name = "ryu";
+// ninja.age = "30"
+// ninja.skills = ["fighting", "sneaking"]; // When we define the object, we cannot add additional property to it
+// You can rewrite the complete object but it should have the same property as already defined above
+/* ninja = {   // This is wrong as age property is missing in the object and does not match with the initial structure of the object
+  name: "yoshi",
+  belt: "orange",
+}; */
+// ninja = {  // This is also wrong
+//   name: 20,
+//   belt: "black",
+//   age: 30,
+// };
--- TypeScript2/sandbox.ts
+++ TypeScript2/sandbox.ts
@@ -1,3 +1,5 @@
+/* 1. Types Basic */
+
 let character = "mario";
 let age = 30;
 let isBlackBelt = false;
@@ -17,3 +19,42 @@ const circ = (diameter: number) => {
 };
 
 console.log(circ(7.5));
+
+/* 2. Objects & Arrays */
+
+// arrays
+let names = ["luigi", "yoshi", "yoshi"];
+
+names.push("toad");
+// names.push(3); // You can not insert a number into a arrays already containing String value
+// names[0] = 3   // You can not reassign a value into a arrays already with different types
+
+// Mixed Arrays
+let mixed = ["ken", 4, "chun-li", 8, 9]; // Array containing string and number
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+
+// Objects
+let ninja = {
+  name: "mario",
+  belt: "black",
+  age: 30,
+};
+
+ninja.age = 40;
+ninja.name = "ryu";
+// ninja.age = "30"
+// ninja.skills = ["fighting", "sneaking"]; // When we define the object, we cannot add additional property to it
+
+// You can rewrite the complete object but it should have the same property as already defined above
+/* ninja = {   // This is wrong as age property is missing in the object and does not match with the initial structure of the object
+  name: "yoshi",
+  belt: "orange",
+}; */
+
+// ninja = {  // This is also wrong
+//   name: 20,
+//   belt: "black",
+//   age: 30,
+// };

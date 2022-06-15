56d850bc061eadfce5add358aac2ecf3fb29511a
nadeemasgar1 <nadeem.asgar@tokopedia.com>
2022-06-15 23:36:25 +0530

    Classes

--- TypeScript2/app.js
+++ TypeScript2/app.js
@@ -15,7 +15,25 @@ var type = document.querySelector("#type");
 var toform = document.querySelector("#tofrom");
 var details = document.querySelector("#details");
 var amount = document.querySelector("#amount");
-form1.add("submit", function (e) {
+form1.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log(type.value, toform.value, details.value, amount.valueAsNumber);
 });
+// Classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return "".concat(this.client, " owes $").concat(this.amount, " for ").concat(this.details);
+    };
+    return Invoice;
+}());
+var invOne = new Invoice("mario", "work on the mario website", 250);
+var invTwo = new Invoice("luigi", "work on the luigi website", 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
--- TypeScript2/app.ts
+++ TypeScript2/app.ts
@@ -22,8 +22,35 @@ const toform = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
-form1.add("submit", (e: Event) => {
+form1.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
   console.log(type.value, toform.value, details.value, amount.valueAsNumber);
 });
+
+// Classes
+
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+console.log(invoices);

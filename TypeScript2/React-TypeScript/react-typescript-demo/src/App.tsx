c8c712674722648bb299e7f1b1ad6987994daa87
nadeemasgar1 <nadeem.asgar@tokopedia.com>
2022-06-16 07:15:24 +0530

    useReducer Strict Action Types

--- TypeScript2/React-TypeScript/react-typescript-demo/src/App.tsx
+++ TypeScript2/React-TypeScript/react-typescript-demo/src/App.tsx
@@ -7,6 +7,7 @@ import { Input } from "./components/Input";
 import { Oscar } from "./components/Oscar";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
+import { Counter } from "./components/state/counter";
 import { LoggedIn } from "./components/state/LoggedIn";
 import { User } from "./components/state/User";
 import { Status } from "./components/Status";
@@ -43,6 +44,7 @@ function App() {
       <Container styles={{ border: "1px solid black", padding: "1rem" }} />
       <LoggedIn />
       <User />
+      <Counter />
     </div>
   );
 }
--- TypeScript2/React-TypeScript/react-typescript-demo/src/components/state/counter.tsx
+++ TypeScript2/React-TypeScript/react-typescript-demo/src/components/state/counter.tsx
@@ -1,13 +1,20 @@
+// Discriminated Unions in TypeScript and is recommended approach for reducer function
 import { useReducer } from "react";
 
 type CounterState = {
   count: number;
 };
 
-type CounterAction = {
-  type: string;
+type UpdateAction = {
+  type: "increment" | "decrement";
   payload: number;
 };
+
+type ResetAction = {
+  type: "reset";
+};
+
+type CounterAction = UpdateAction | ResetAction;
 
 const initialState = { count: 0 };
 
@@ -17,6 +24,8 @@ function reducer(state: CounterState, action: CounterAction) {
       return { count: state.count + action.payload };
     case "decrement":
       return { count: state.count - action.payload };
+    case "reset":
+      return initialState;
     default:
       return state;
   }
@@ -34,6 +43,7 @@ export const Counter = () => {
       <button onClick={() => dispatch({ type: "decrement", payload: 10 })}>
         Decrement 10
       </button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </>
   );
 };

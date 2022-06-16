caf3d7639abb249b0215d3bfecc448d5de75d6ce
nadeemasgar1 <nadeem.asgar@tokopedia.com>
2022-06-16 06:39:17 +0530

    useState Future Value

--- TypeScript2/React-TypeScript/react-typescript-demo/src/App.tsx
+++ TypeScript2/React-TypeScript/react-typescript-demo/src/App.tsx
@@ -8,6 +8,7 @@ import { Oscar } from "./components/Oscar";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
 import { LoggedIn } from "./components/state/LoggedIn";
+import { User } from "./components/state/User";
 import { Status } from "./components/Status";
 
 function App() {
@@ -41,6 +42,7 @@ function App() {
       <Input value="" handleChange={(event) => console.log(event)} />
       <Container styles={{ border: "1px solid black", padding: "1rem" }} />
       <LoggedIn />
+      <User />
     </div>
   );
 }

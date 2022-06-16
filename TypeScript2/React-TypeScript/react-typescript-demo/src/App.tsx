2de97251bf00cd751fd624d6de1b62b975b04ad8
nadeemasgar1 <nadeem.asgar@tokopedia.com>
2022-06-16 05:44:49 +0530

    Style Props

--- TypeScript2/React-TypeScript/react-typescript-demo/src/App.tsx
+++ TypeScript2/React-TypeScript/react-typescript-demo/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Button } from "./components/Button";
+import { Container } from "./components/Container";
 import { Greet } from "./components/Greet";
 import { Heading } from "./components/Heading";
 import { Input } from "./components/Input";
@@ -37,6 +38,7 @@ function App() {
         }}
       ></Button>
       <Input value="" handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
     </div>
   );
 }
---
+++ TypeScript2/React-TypeScript/react-typescript-demo/src/components/Container.tsx
@@ -0,0 +1,8 @@
+// Typing Style Props
+type ContainerProps = {
+  styles: React.CSSProperties;
+};
+
+export const Container = (props: ContainerProps) => {
+  return <div style={props.styles}>Text Content goes here</div>;
+};
